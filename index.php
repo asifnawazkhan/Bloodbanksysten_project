e1a244583696c24d3c68cac68e54e255e4c17783
AsifNawazkhan <asifnawazkhan728@gmail.com>
2023-06-27 02:15:46 +0500

    changes

--- index.php
+++ index.php
@@ -176,8 +176,8 @@ h5.text-center{
 									<option value="B-">B-</option>
 									<option value="AB+">AB+</option>
 									<option value="AB-">AB-</option>
-									<option value="O+">O+</option>
-									<option value="O-">O-</option>
+									<option value="O+">O-</option>
+									<option value="O-">O+</option>
 
 								</select>
 							</div>
